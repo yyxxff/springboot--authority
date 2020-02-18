649787bf38401f6ec54113a65d65696b83115a7b
俞鑫枫 <543013487@qq.com>
2020-02-18 14:36:58 +0800

    test

--- src/main/java/com/maple/authority/configuration/shiro/JWTFilter.java
+++ src/main/java/com/maple/authority/configuration/shiro/JWTFilter.java
@@ -3,6 +3,7 @@ package com.maple.authority.configuration.shiro;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
+import sun.awt.geom.AreaOp;
 
 import javax.servlet.Filter;
 import javax.servlet.ServletRequest;
@@ -13,6 +14,19 @@ import static com.maple.authority.configuration.BeanInitConfig.jwtProperties;
 
 @Slf4j
 public class JWTFilter extends BasicHttpAuthenticationFilter implements Filter {
+
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        this.isLoginRequest(request,response);
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        // 查看当前Header中是否携带Authorization属性(Token)，有的话就进行登录认证授权
+        if (StringUtils.isNotBlank(httpServletRequest.getHeader("Authorization"))) {
+
+        }
+        this.executeLogin(request, response);
+
+        return super.isAccessAllowed(request, response, mappedValue);
+    }
 
     @Override
     protected boolean preHandle(ServletRequest request, ServletResponse response) throws Exception {
