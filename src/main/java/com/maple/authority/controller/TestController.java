202bfefb89dbcd0d075e783af3f5d692c12cf0c0
俞鑫枫 <543013487@qq.com>
2020-02-19 10:08:24 +0800

    添加测试实例

--- src/main/java/com/maple/authority/controller/TestController.java
+++ src/main/java/com/maple/authority/controller/TestController.java
@@ -1,22 +1,26 @@
 package com.maple.authority.controller;
 
+import com.maple.authority.utils.JwtUtils;
 import com.maple.authority.utils.ResultBean;
+import org.apache.shiro.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
     @GetMapping("/auth/login")
-    public ResultBean<String> testResultBean() {
-        String result = "这是一个测试";
-        return ResultBean.getInstance().success(result);
+    public ResultBean<String> testResultBean(@RequestParam("appId") String appId, @RequestParam("appKey") String appKey) {
+        String token = JwtUtils.generateToken(appId, appKey);
+        return ResultBean.getInstance().success(token);
     }
 
-    @GetMapping("/auth/test2")
+    @GetMapping("/test2")
     public ResultBean<Integer> testResultBean2() {
-        Integer result = 1;
-        return ResultBean.getInstance().error(result);
+        String user = (String) SecurityUtils.getSubject().getPrincipal();
+        return ResultBean.getInstance().success(user);
     }
 
     @GetMapping("/test3")
